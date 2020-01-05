62cfafe6548a40002aaa3a6ef86101ff3ab1a0cb
Eduard Castro <eduard.cas07@gmail.com>
2020-01-05 12:58:00 -0500

    Create structure base

---
+++ Java Refresher/Practice Exercises/Exercise_01.java
@@ -0,0 +1,11 @@
+public class Exercise_01 {
+  public static void main(String[] args) {
+    System.out.println(Exercise_01.findDuplicate("abcabc"));
+  }
+
+  public static int findDuplicate(String input) {
+    
+
+    return 1;
+  }
+}
